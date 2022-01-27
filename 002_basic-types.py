7a2cd228c4c0de4c966b006ed85287b27088bdc1
provasko <80074452+provasko@users.noreply.github.com>
2022-01-27 22:47:54 +0100

    Create 002_basic-types.py

---
+++ 002_basic-types.py
@@ -0,0 +1,20 @@
+# целые числа - int
+# вещественные числа - float
+# логические - bool
+# строки - str
+print(int(2.3))
+print(float(10))
+
+# определить тип объекта
+print(type(5))
+
+# переменные и динамическая типизация
+a = 10
+a = "lol"
+a = True
+print(type(a))
+
+# ввод данных
+b = input()
+c = int(input("Enter number 1...5"))
+print(b, c)

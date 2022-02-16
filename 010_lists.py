498cc6cf3ec45e316807d04305ed058405c4c63b
provasko <80074452+provasko@users.noreply.github.com>
2022-02-16 21:59:27 +0100

    Create 010_lists.py

---
+++ 010_lists.py
@@ -0,0 +1,11 @@
+students = ["Ivan", "Masha", "Sasha"]
+for student in students:
+    print("Hello, " + student + "!")
+
+print(students[1])
+
+# Access to elements
+
+print(len(students))
+print(students[:2]) # up to 2nd (number 1)
+print(students[::-1]) # mirror

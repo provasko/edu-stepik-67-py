f6719494a317c7e7f92fad16b085a3634ed5f4ec
provasko <80074452+provasko@users.noreply.github.com>
2022-02-16 22:11:59 +0100

    Update 010_lists.py

--- 010_lists.py
+++ 010_lists.py
@@ -9,3 +9,54 @@ print(students[1])
 print(len(students))
 print(students[:2]) # up to 2nd (number 1)
 print(students[::-1]) # mirror
+
+# Operations
+teachers = ["Vova", "Alex"]
+print(students + teachers)
+print(teachers * 3)
+
+# Adding elements
+students.append("Seva")
+students += ["Olga"]
+students.insert(2, "Vsevolod")
+
+print(students)
+
+
+students = [] # empty
+''' From python.org:
+s[i] = x
+item i of s is replaced by x
+s[i:j] = t
+slice of s from i to j is replaced by the contents of the iterable t
+del s[i:j]
+same as s[i:j] = []
+s[i:j:k] = t
+the elements of s[i:j:k] are replaced by those of t
+(1)
+del s[i:j:k]
+removes the elements of s[i:j:k] from the list
+s.append(x)
+appends x to the end of the sequence(same as s[len(s):len(s)]=[x])
+s.clear()
+removes all items from s(same as del s[:])
+(5)
+s.copy()
+creates a shallow copy of s(same as s[:])
+(5)
+s.extend(t) or s += t
+extends s with the contents of t(for the most part the same as s[len(s):len(s)]=t)
+s *= n
+updates s with its contents repeated n times
+(6)
+s.insert(i, x)
+inserts x into s at the index given by i(same as s[i:i]=[x])
+s.pop() or s.pop(i)
+retrieves the item at i and also removes it from s
+(2)
+s.remove(x)
+remove the first item from s where s[i] is equal to x
+(3)
+s.reverse()
+reverses the items of s in place
+'''

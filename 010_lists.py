7f8421d6b0336f69132eb02c281ba83f42fd8464
provasko <80074452+provasko@users.noreply.github.com>
2022-02-28 22:34:33 +0100

    Update 010_lists.py

--- 010_lists.py
+++ 010_lists.py
@@ -129,6 +129,12 @@ for j in ryad2:
 
 chisla = [int(i) for i in input().split()]
 chisla.sort()
-for i in range(1, len(chisla)):
-    if chisla[i-1] == chisla[i]:
-        print(chisla[i], end=" ")
+if len(chisla) == 1:
+    # nothing to print
+elif len(chisla) == 2:
+    if chisla[1] == chisla[0]:
+        print(chisla[0])
+else:  
+    for i in range(1, len(chisla)):
+        if chisla[i-1] == chisla[i]:
+            print(chisla[i], end=" ")

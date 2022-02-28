b48c4db0fb06f214da5dabc246702bf9f04128d2
provasko <80074452+provasko@users.noreply.github.com>
2022-02-28 23:17:23 +0100

    Update 011_2lists.py

---
+++ 011_2lists.py
@@ -0,0 +1,16 @@
+# Двухмерные списки
+from tkinter import N
+
+
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(a[1])
+print(a[1][1])
+print(a[0][2])
+
+# Генерация
+
+b = [[0] * n] * n
+
+c = [[0] * n for i in range(n)]
+d = [[0 for j in range(n)] for i in range(n)]

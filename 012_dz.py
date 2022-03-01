d3d814699e688ce016475cef482b3819f0558627
provasko <80074452+provasko@users.noreply.github.com>
2022-03-01 23:41:27 +0100

    Update 012_dz.py

--- 012_dz.py
+++ 012_dz.py
@@ -5,3 +5,17 @@ m = a[0]
 for x in a:
     if m > x:
         m = x
+
+# Сапер
+n, m, k = (int(i) for i in input().split())
+a = [[0 for j in range(m)] for i in range(n)]
+for i in range(k):
+    row, col = (int(i) for i in input().split())
+    a[row][col] = -1
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == 0:
+            for di in range(-1, 2):
+                for dj in range(-1, 2):
+                    ai = i + di
+                    aj = j + dj

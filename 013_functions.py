9b0924a24d6ea741e9cd59791f259f0edc6d7b77
provasko <80074452+provasko@users.noreply.github.com>
2022-03-11 23:51:29 +0100

    Update 013_functions.py

--- 013_functions.py
+++ 013_functions.py
@@ -1,9 +1,12 @@
+# объявление функции
 def min2(a, b):
     if a <= b:
         return a
     else:
         return b
 
+
+# вызов функции
 # could be used twice
 m = min2(42, 30)
 mm = min2(min2(43, 78), 31)

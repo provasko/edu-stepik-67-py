ed9b14e032137a489e95e12ba4499685dd1a32ca
provasko <80074452+provasko@users.noreply.github.com>
2022-03-15 19:57:57 +0100

    Update 013_functions.py

--- 013_functions.py
+++ 013_functions.py
@@ -31,3 +31,18 @@ def min(*a):
     return m
 
 # Значение параметров по умолчанию
+
+def my_range(start, stop, step=1):
+    res = []
+    if step > 0:
+        x = start
+        while x < stop:
+            res += [x]
+            x += step
+    elif step < 0:
+        x = start
+        while x > stop:
+            res += [x]
+            x += step
+    return res
+

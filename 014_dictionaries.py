02ef3a251a35d5fb5035589bfd6216171348b1a4
provasko <80074452+provasko@users.noreply.github.com>
2022-03-22 23:10:34 +0100

    Update 014_dictionaries.py

---
+++ 014_dictionaries.py
@@ -0,0 +1,9 @@
+# Sets
+
+s = set()  # Создание пустого множества
+basket = {"apple", "orange", "apple", "pear", "orange", "banana"}
+
+print(basket)  # выводит в другом порядке без повторений
+
+"orange" in basket  # True
+"cucumber" in basket  # False

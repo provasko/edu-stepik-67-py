70678a13a6d2926c2bd7d4630398dde82593e8a6
provasko <80074452+provasko@users.noreply.github.com>
2022-03-22 23:15:25 +0100

    Update 014_dictionaries.py

--- 014_dictionaries.py
+++ 014_dictionaries.py
@@ -7,3 +7,11 @@ print(basket)  # выводит в другом порядке без повто
 
 "orange" in basket  # True
 "cucumber" in basket  # False
+
+basket.add("kiwi")  # Добавляет элемент
+
+basket.remove("orange")  # Удаляет элемент, но если такого нет - ошибка!
+
+basket.discard("grape")  # Тоже удаление, но несовпадение не вызовет ошибку
+
+basket.clear()  # Удаляет все элементы из множества

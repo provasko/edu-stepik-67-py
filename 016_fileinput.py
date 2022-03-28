50b902e4a4ceb85a60cfd17d38b93596c257eeca
provasko <80074452+provasko@users.noreply.github.com>
2022-03-28 23:05:41 +0200

    Update 016_fileinput.py

--- 016_fileinput.py
+++ 016_fileinput.py
@@ -10,3 +10,31 @@ inf.close()
 with open("text.txt") as inf:
     s1 = inf.readline()
     s2 = inf.readline()
+
+# здесь файл уже закрыт
+
+s = inf.readline().strip()
+"\t abc  \n".strip()  # "abc"
+
+os.path.join(".", "dirname", "filename.txt")
+# "./dirname/filename.txt"
+
+# Построчное чтение файла
+
+with open("input.txt") as inf:
+    for line in inf:
+        line = line.strip()
+        print(line)
+
+# Запись в файл
+
+ouf = open("file.txt", "w")
+ouf.write("Some text\n")
+ouf.write(str(25))
+ouf.close()
+
+with open("text.txt", "w") as ouf:
+    ouf.write("Some text\n")
+    ouf.write(str(25))
+
+# Здесь файл уже закрыт

abeb0db2444f2aac875176bd4d9c0c7327e17878
provasko <80074452+provasko@users.noreply.github.com>
2022-04-07 22:50:35 +0200

    Update 017_modules.py

--- 017_modules.py
+++ 017_modules.py
@@ -6,6 +6,7 @@
 # Импорт модуля  
 
 # Рассмотрим файл my_module.py. Пусть в нем описана функция foo()
+from asyncio import subprocess
 import my_module
 my_module.foo()
 
@@ -20,4 +21,16 @@ foo()
 from my_module import foo as my_foo
 my_foo()
 
-# стандартные модули docs.python.org/3/library/
+# стандартные модули docs.python.org/3/library/ 
+# docs.python.org/3/tutorial/modules.html 
+# ibm.com/developerworks/ru/library/l-python_part_5/
+
+# Аргументы командной строки
+# Модуль sys
+import sys
+print(len(sys.argv))
+
+# Запуск внешних процессов
+# Модуль subprocess
+
+subprocess.call(["python", "-h"])

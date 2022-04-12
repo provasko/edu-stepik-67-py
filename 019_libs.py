5715e3976ed1a2ae8b7eeae5335829beb84668fa
provasko <80074452+provasko@users.noreply.github.com>
2022-04-12 23:55:00 +0200

    Create 019_libs.py

---
+++ 019_libs.py
@@ -0,0 +1,14 @@
+# Библиотеки для анализа данных. NumPy
+
+'''
+NumPy - работа с числовыми массивами.
+matplotlib - построение графиков
+SciPy - функции для научных вычислений
+pandas - таблицы
+Scikit-learn - алгоритмы машинного обучения
+PythonSeaborn - визуализация стат данных
+'''
+'''
+Примеры графиков http://matplotlib.org.gallery.html
+Py для научных вычислений: https://github.com/jrjohansson/scientific-python-lectures
+'''
